82721a4ae12d68d2901d6b0d8fe826ee00deff7d
Diana - Coding Nannah <107065069+CodingNannah@users.noreply.github.com>
2024-05-30 17:21:37 -0600

    Create main.py
    First day - basic coding

---
+++ main.py
@@ -0,0 +1,12 @@
+# Basic coding
+
+# navigate to trinket.io/turtle
+# clear all text
+# type
+print("Hello, World!")
+
+# Following code does not work in trinket.io/turtle
+# Output: ('Challenge'#, 5)
+# works in trinket.io/features/python3
+# Output: Challenge # 5
+print("Challenge #", 2+3)
